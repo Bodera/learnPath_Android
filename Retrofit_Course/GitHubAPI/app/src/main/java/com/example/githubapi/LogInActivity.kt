d144feef32f2e3701c999433af5047328088203b
Bode <rafaelrafa990@gmail.com>
2020-01-12 23:48:42 -0300

    First upload app code
    Adapting Java to Kotlin. Add dimens.xml to values folder. Need to finish the getUser() method.

---
+++ Retrofit_Course/GitHubAPI/app/src/main/java/com/example/githubapi/LogInActivity.kt
@@ -0,0 +1,34 @@
+package com.example.githubapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
+
+class LogInActivity : AppCompatActivity() {
+val i = Intent
+    private val logIn:Button by lazy{
+        findViewById<Button>(R.id.btn_login)
+    }
+
+    private val inputUserName:EditText by lazy{
+        findViewById<EditText>(R.id.input_username)
+    }
+
+    //
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        logIn.setOnClickListener() {}
+    }
+
+    /*
+    fun getUser(view: View) : Unit {
+        i = Intent(LogInActivity.this, UserActivity.class)
+    }
+    */
+}
